f9bfa0d160ff8e4f1ad0839ce8781044bf7208df
machadopy <vitorandroidcwb@gmail.com>
2026-09-17 00:49:59 -0300

    Refactor AdicionarUsuario to use INITIAL_STATE and handle user addition response; update Usuarios to fetch and delete user from API

--- src/components/Usuarios/Usuarios.js
+++ src/components/Usuarios/Usuarios.js
@@ -23,9 +23,17 @@ class Usuarios extends Component {
 
   removerUsuario(usuario) {
     if (window.confirm(`Tem certeza que deseja remover "${usuario.nome} ${usuario.sobrenome}"?`)) {
-      let usuarios = this.state.usuarios
-      usuarios = usuarios.filter(x => x.id !== usuario.id)
-      this.setState({ usuarios: usuarios })
+
+      fetch(
+        `https://reqres.in/api/users/${usuario.id}`,{
+        method: 'delete'
+        }
+      ).then(resposta => {
+        let usuarios = this.state.usuarios
+        usuarios = usuarios.filter(x => x.id !== usuario.id)
+        this.setState({ usuarios: usuarios })
+          
+      })
     }
   }
 
